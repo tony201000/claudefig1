8d63bfcbb29e20c6cdfb2bbae025b41796d62312
tony201000 <tony20100@gmail.com>
2024-07-21 17:28:17 +0700

    Update firebase-config.js

--- src/js/firebase-config.js
+++ src/js/firebase-config.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";  // Add the Firebase products that you want to use
+import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,12 +16,16 @@ const firebaseConfig = {
   projectId: "blindboxcollection-6a548",
   storageBucket: "blindboxcollection-6a548.appspot.com",
   messagingSenderId: "336289874791",
-  appId: "1:336289874791:web:3f4f537bdaeffe9e76a5c2",
-  measurementId: "G-GSNNFF3BWV"
+  appId: "1:336289874791:web:3f4f537bdaeffe9e76a5c2"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
 
+export { db, storage };
+export default auth;
+export { app, analytics };  
